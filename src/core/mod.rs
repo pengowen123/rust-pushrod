d1064c5b1cb6e121e2a43c86405a5e34707e26a4
Ken Suenobu <ksuenobu@fastmail.com>
2018-12-27 19:51:05 -0600

    Starting to rearrange some things.

--- src/core/mod.rs
+++ src/core/mod.rs
@@ -1,4 +1,4 @@
-// Pushrod
+// Core module
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -11,13 +11,3 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
-pub mod core;
-
-pub mod rect;
-pub mod point;
-pub mod size;
-
-pub mod widget;
-
-pub mod event;
