0e8f314b98e75ff4618a94071f3ae497a0cc3983
Ken Suenobu <ksuenobu@fastmail.com>
2019-01-01 01:04:34 -0600

    Updates to include run function.

--- src/core/main.rs
+++ src/core/main.rs
@@ -13,11 +13,12 @@
 // limitations under the License.
 
 use piston_window::*;
+use opengl_graphics::GlGraphics;
 
 struct Config {
     window_width: u32,
     window_height: u32,
-    window_fps: u16,
+    window_fps: u64,
 }
 
 impl Config {
@@ -38,6 +39,33 @@ impl Pushrod {
     fn new(config: Config) -> Self {
         Self {
             config,
+        }
+    }
+
+    fn run(&self) {
+        let opengl = OpenGL::V3_2;
+
+        let mut window: PistonWindow = WindowSettings::new(
+            "Pushrod Window",
+            [self.config.window_width, self.config.window_height]
+        )
+            .opengl(opengl)
+//            .samples(4)
+//            .exit_on_esc(true)
+//            .fullscreen(true)
+            .build()
+            .unwrap_or_else(|error| panic!("Failed to build PistonWindow: {}", error));
+
+        window.set_max_fps(self.config.window_fps);
+        window.set_ups(self.config.window_fps);
+
+        let mut gl = GlGraphics::new(opengl);
+
+        while let Some(event) = window.next() {
+            if let Some(args) = event.render_args() {
+                // Draw loop
+//                self.handle_draw_loop(&gl);
+            }
         }
     }
 }
