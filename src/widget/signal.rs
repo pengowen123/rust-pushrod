ee75ed611e0f35351e6236faddb8d3c936b502f2
Ken Suenobu <ksuenobu@fastmail.com>
2019-01-15 22:49:00 -0600

    Added initial signals for widgets.

--- src/widget/signal.rs
+++ src/widget/signal.rs
@@ -12,4 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-pub enum PushrodWidgetSignal {}
+pub enum PushrodWidgetSignal {
+    MouseEnter,
+    MouseExit,
+    MouseClick,
+}
