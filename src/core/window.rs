7f35c6d37a7d7606ac00354cf836845a8a0abff5
Ken Suenobu <ksuenobu@fastmail.com>
2019-01-22 22:29:51 -0600

    Updated to include TODO on what I intend to do here.

--- src/core/window.rs
+++ src/core/window.rs
@@ -14,6 +14,8 @@
 // limitations under the License.
 
 use crate::widget::widget::*;
+use crate::core::point::*;
+
 use piston_window::*;
 
 pub struct PushrodWindow {
@@ -31,5 +33,14 @@ impl PushrodWindow {
 
     pub fn add_widget(&mut self, widget: Box<dyn PushrodWidget>) {
         self.widgets.push(widget);
+    }
+
+    pub fn get_widget_id_for_point(&mut self, point: Point) -> u8 {
+        for pos in 0..self.widgets.len() {
+            // TODO Convert Box<dyn PushrodWidget> to PushrodWidget object that can be called
+            // TODO Call PushrodWidget.get_point() and PushrodWidget.get_size() here
+        }
+
+        0
     }
 }
