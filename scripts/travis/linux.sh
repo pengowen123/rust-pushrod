ab58ebb22caed697b0a3ea3fe3275cc757956265
Ken Suenobu <ksuenobu@fastmail.com>
2019-05-15 22:31:52 -0600

    Updated rules for travis CI

---
+++ scripts/travis/linux.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+#
+# Build script for building on Travis CI in Linux
+
+sudo apt-get update
+sudo apt-get install -y cmake xorg-dev libglu1-mesa-dev libglfw3 libglfw3-dev xorg-dev
+
---
+++ scripts/travis/osx.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+#
+# Build script for Travis CI on Mac
+
+brew install cmake
+brew install glfw3
+
