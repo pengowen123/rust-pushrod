36d28cbc244efd6dc2889ef012d92a93b28f05f2
Ken Suenobu <ksuenobu@fastmail.com>
2019-05-29 23:26:08 -0600

    Updated code to specify imports by hand.

--- src/widget/text_widget.rs
+++ src/widget/text_widget.rs
@@ -15,9 +15,12 @@
 
 extern crate graphics;
 
-use piston_window::*;
+use piston_window::{TextureSettings, Context};
 use opengl_graphics::{GlGraphics, GlyphCache};
 use graphics::character::CharacterCache;
+use graphics::draw_state::DrawState;
+use graphics::text;
+use graphics::Transformed;
 
 use crate::widget::config::*;
 use crate::widget::widget::*;
